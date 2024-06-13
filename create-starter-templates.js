5eb5738d354ab451e065c259007842a10bc49885
Eric Agnitsch <ericagnitsch.dev@gmail.com>
2024-06-12 23:51:23 -0400

    Issues with the typescript version of the script, using javascript instead

--- create-starter-templates.js
+++ create-starter-templates.js
@@ -1,9 +1,35 @@
-const projectName = process.argv[2];
-if (!projectName) {
+#!/usr/bin/env node
+
+const Commander = require('commander');
+
+const program = new Commander.Command();
+
+program
+  .option('--express', 'Create an Express project')
+  .option('--expo', 'Create an Expo project')
+  .option('--nextjs', 'Create a Next.js project')
+  .parse(process.argv);
+
+const options = program.opts();
+
+if (!options.express && !options.expo && !options.nextjs) {
   console.error(
-    `Please specify the project name: npx @autom8te/create-starter-templates@latest <project-name>`
+    'You must specify at least one project type: --express, --expo, --nextjs'
   );
   process.exit(1);
 }
 
-console.log('test');
+if (options.express) {
+  console.log('create express project');
+  // createExpressProject();
+}
+
+if (options.expo) {
+  console.log('create expo project');
+  // createExpoProject();
+}
+
+if (options.nextjs) {
+  console.log('create nextjs project');
+  // createNextJsProject();
+}
