cf96bad2c838ff6ce62d57e2bd9b9c2bee0a93d2
Eric Agnitsch <ericagnitsch.dev@gmail.com>
2024-06-18 00:46:32 -0400

    Added supabase auth context and provider for the whole app

--- templates/nextjs/src/app/providers.tsx
+++ templates/nextjs/src/app/providers.tsx
@@ -1,7 +1,12 @@
 'use client';
 
+import { AuthProvider } from '@/contexts/AuthContext';
 import { NextUIProvider } from '@nextui-org/react';
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <NextUIProvider>{children}</NextUIProvider>;
+  return (
+    <NextUIProvider>
+      <AuthProvider>{children}</AuthProvider>
+    </NextUIProvider>
+  );
 }
