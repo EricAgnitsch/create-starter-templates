a6298f9ff58814c57661629e1a62dbbd92abeb6e
Eric Agnitsch <ericagnitsch.dev@gmail.com>
2024-06-18 00:53:50 -0400

    added yup and react-hook-form validation handling examples, a new login page, and some basic redirect examples with auth flow

--- templates/nextjs/src/app/page.tsx
+++ templates/nextjs/src/app/page.tsx
@@ -1,15 +1,37 @@
+'use client';
+
+import { useAuth } from '@/contexts/AuthContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Button } from '@nextui-org/react';
 import '@icons';
+import { Button, Spinner } from '@nextui-org/react';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Home() {
+  const router = useRouter();
+  const { user, authLoading } = useAuth();
+
+  useEffect(() => {
+    if (!authLoading && !user) {
+      router.replace('/login');
+    }
+  }, [user, authLoading]);
+
   return (
-    <main
-      className={'flex min-h-screen flex-col items-center justify-between p-24'}
-    >
-      <Button className={'bg-blue-300'}>
-        <FontAwesomeIcon icon={'home'} />
-      </Button>
-    </main>
+    <>
+      {user ? (
+        <main
+          className={
+            'flex min-h-screen flex-col items-center justify-between p-24'
+          }
+        >
+          <Button className={'bg-blue-300'}>
+            <FontAwesomeIcon icon={'home'} />
+          </Button>
+        </main>
+      ) : (
+        <Spinner className={'w-full min-h-screen'} size={'lg'} />
+      )}
+    </>
   );
 }
