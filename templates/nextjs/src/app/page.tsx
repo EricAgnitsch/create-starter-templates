d024be26907cf45b4e3c59aeeb3848718fb4de14
Eric Agnitsch <ericagnitsch.dev@gmail.com>
2024-06-18 00:09:16 -0400

    Added fontawesome support to nextjs

--- templates/nextjs/src/app/page.tsx
+++ templates/nextjs/src/app/page.tsx
@@ -1,11 +1,15 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Button } from '@nextui-org/react';
+import '@icons';
 
 export default function Home() {
   return (
     <main
       className={'flex min-h-screen flex-col items-center justify-between p-24'}
     >
-      <Button className={'bg-blue-300'}>test</Button>
+      <Button className={'bg-blue-300'}>
+        <FontAwesomeIcon icon={'home'} />
+      </Button>
     </main>
   );
 }
---
+++ templates/nextjs/src/icons.ts
@@ -0,0 +1,7 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+
+// ** Import this file (import '@icons';) at top of component files to use font awesome icons **
+// Add additional font awesome icons to the library here
+library.add(faHome);
